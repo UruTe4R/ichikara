fedc1d5d94d2fd85660285fc2bffbcfdcfd9d13a
urutear <keitadance0208@gmail.com>
2025-06-10 02:21:39 +0900

    complete ProductImageSelectin gallery

--- app/ui/ProductImageSelection.tsx
+++ app/ui/ProductImageSelection.tsx
@@ -5,6 +5,8 @@ import styles from './ProductImageSelection.module.css';
 import { useState } from 'react';
 import Image from 'next/image';
 
+// 一応画像の数は８個制限にしてある。
+
 
 const images = [
     {
@@ -69,6 +71,18 @@ const images = [
 export default function ProductImageSelection() {
 
   const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  function handleArraowClick(side: 'right' | 'left') {
+    const currentIndex = images.findIndex((img) => img.id === selectedImage.id)
+    if (side === 'right') {
+      setSelectedImage(images[(currentIndex + 1) % images.length]);
+      
+    } else if (side === 'left') {
+      setSelectedImage(images[(currentIndex - 1 + images.length) % images.length]);
+      
+    }
+    
+  }
   
 
 
@@ -76,29 +90,49 @@ export default function ProductImageSelection() {
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <Image 
-          src='/images/cabin_2.jpg'
-          alt='beach'
-          width={1280}
-          height={720}
-          className={styles.selectedImage}
+          src={selectedImage.src}
+          alt={selectedImage.alt || 'image of product'}
+          width={selectedImage.width || 1280}
+          height={selectedImage.height || 720}
+          className={`${styles.selectedImage}`}
           />
-        </div>
 
-        <div className={styles.imageGallery}>
-          {images.map((image) => {
-            return (
-              <div key={image.id} className={styles.imageGalleryItem}>
-                <Image 
-                  src={image.src}
-                  alt={image.alt}
-                  width={image.width}
-                  height={image.height}
-                  className={styles.productImage}
-                  />
-              </div>
-            )
-          })}
-        </div>
+        <button className={styles.imageButtonLeft} onClick={() => handleArraowClick('left')}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.arrow}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        </button>
+        <button className={styles.imageButtonRight} onClick={() => handleArraowClick('right')}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.arrow}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+
+        </button>
+
+      </div>
+
+
+      <div className={styles.imageGallery}>
+        {images.map((image) => {
+          return (
+            <div 
+            key={image.id} className={`${styles.imageGalleryItem} ${selectedImage.id === image.id ? styles.selected : ''}`}
+            onClick={() => setSelectedImage(image)}
+            >
+              <Image 
+                src={image.src}
+                alt={image.alt}
+                width={image.width}
+                height={image.height}
+                className={styles.productImage}
+                onMouseEnter={() => setSelectedImage(image)}
+                />
+            </div>
+          )
+        })}
+      </div>
+
+        
     </div>
   )
 }
